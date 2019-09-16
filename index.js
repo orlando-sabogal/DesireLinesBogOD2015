68bea5d9613e045d6cbdb6baef2d9b8860fa7dc5
“orlando-sabogal” <orlando.asabogal@gmail.com>
2019-09-15 21:06:09 -0500

    dummy chord

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ Promise.all([promise1, promise2])
     });
 
     functionRender(BogotaZats, MyData);
-
+    renderChord(BogotaZats, MyData);
 
   //Here starts the whole sectio of listening the buttons
   //How to optimize this?
@@ -227,3 +227,92 @@ function clicked(Zat, MyData, BogotaZats, path) {
                          OrigenDestino, ZatsDestinos,
                          DestinoOrigen, ZatsOrigenes) // From renderFunctions.js
 }
+
+
+//Lets try to make a Chord!
+
+function renderChord(BogotaZats, MyData) {
+
+///Not Working
+  // DataChord = [];
+  //
+  // MyData.map( (d,i) => {
+  //
+  //   DataTemp = {
+  //     Source: d.ZatOrigin,
+  //     Target: d.ZatDestination,
+  //     Value: d.Value
+  //   };
+  // DataChord.push(DataTemp);
+  // });
+  //
+  // var mpr = chordMpr(DataChord);
+  //
+  //
+  // mpr
+  //   .addValuesToMap('Source')
+  //   .addValuesToMap('Target')
+  // //   .setFilter(function(row, a, b) {
+  // //       return (row.root === a.name && row.node === b.name)
+  // //     })
+  // //   .setAccessor(function(recs, a, b) {
+  // //       if (!recs[0]) return 0;
+  // //       return +recs[0].count;
+  // //     });
+  // // drawChords(mpr.getMatrix(), mpr.getMap());
+  // //
+  //
+  // console.log(mpr);
+///Not Working
+
+  var chordPlot = d3.select("#chordPlot")
+    .attr("width", 440)
+    .attr("height", 440)
+  .append("g")
+    .attr("transform", "translate(220,220)")
+
+// create input data: a square matrix that provides flow between entities
+var matrix = [
+  [11975,  5871, 8916, 2868],
+  [ 1951, 10048, 2060, 6171],
+  [ 8010, 16145, 8090, 8045],
+  [ 1013,   990,  940, 6907]
+];
+
+// give this matrix to d3.chord(): it will calculates all the info we need to draw arc and ribbon
+var res = d3.chord()
+    .padAngle(0.05)     // padding between entities (black arc)
+    .sortSubgroups(d3.descending)
+    (matrix)
+
+// add the groups on the inner part of the circle
+chordPlot
+  .datum(res)
+  .append("g")
+  .selectAll("g")
+  .data(function(d) { return d.groups; })
+  .enter()
+  .append("g")
+  .append("path")
+    .style("fill", "grey")
+    .style("stroke", "black")
+    .attr("d", d3.arc()
+      .innerRadius(200)
+      .outerRadius(210)
+    )
+
+// Add the links between groups
+chordPlot
+  .datum(res)
+  .append("g")
+  .selectAll("path")
+  .data(function(d) { return d; })
+  .enter()
+  .append("path")
+    .attr("d", d3.ribbon()
+      .radius(200)
+    )
+    .style("fill", "#69b3a2")
+    .style("stroke", "black");
+
+};
